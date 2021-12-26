3540b409e838a901719f8a778b36b21b57b5babc
Lucas Ennes <lucas.ennes@upperds.com>
2021-12-26 03:47:39 -0300

    configures environment variables

--- repositories/api/src/config/database.js
+++ repositories/api/src/config/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'postgres',
-    database: 'list_db',
-    host: 'localhost',
-    dialect: 'postgres',
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
   },
   test: {
     username: 'root',
--- repositories/api/src/config/env.ts
+++ repositories/api/src/config/env.ts
@@ -1,7 +1,2 @@
 export default {
-  DATABASE_HOST: process.env.DATABASE_HOST,
-  DATABASE_PORT: process.env.DATABASE_PORT,
-  DATABASE_NAME: process.env.DATABASE_NAME,
-  DATABASE_USER: process.env.DATABASE_USER,
-  DATABASE_PASS: process.env.DATABASE_PASS,
 }
